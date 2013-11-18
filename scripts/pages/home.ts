0f1a05e3928445c313ecb0e6ed2050f7b1be98e1
Ishan Anand <github@ishan.org>
2013-11-17 22:52:35 -0800

    Fix the coloring of table entries by removing the empty <TR> elements

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -15,5 +15,29 @@ $("./body") {
   $(".//table[1]") {
     attribute("width", "100%")
   } 
+
+  # Fix the coloring of table entries by removing the empty <TR> elements
+  # This is equivalent to the following lines in the XSLT example:
+  #  
+  # <!-- 
+  #  Remove the empty <tr> elements that serve as spacers. Then foundation's 
+  #  default table stylingthat uses alternate highlighting for rows will work 
+  #  great.
+  #  -->
+  #  <xsl:template match="tr[@style='height:5px']" />
+  remove(".//tr[@style='height:5px']")
+  
+  # Note that in practice a more robust method would be remove the empty
+  # <TR> elements in the articles table rather than just all <TR> elements
+  # with a specific style attribute as both the XSLT and the above code have done.
+  # That would look something like:
+  #
+  # # Find the table with the articles
+  # $("//a[contains(@href, 'vote')]/ancestor::table[1]") {
+  #   # Remove the empty TR rows
+  #   $("./tr[not(*)][not(node())]") {      
+  #     remove()
+  #   }
+  # }
     
 }
