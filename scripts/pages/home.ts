092d4b03b6fd5942d34a8e0ca06518f919855839
Ishan Anand <github@ishan.org>
2013-11-17 22:45:35 -0800

    Fix the width attribute on the first table in the document so that content is edge to edge

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,4 +1,19 @@
 # Homepage Transformation
 $("./body") {
   add_class("_home")
+  
+  # Fix the width attribute on the first table in the document so that content is edge to edge
+  # This is equivalent to the following lines in the XSLT example:
+  #
+  # <!-- Fix the width attribute on the first table in the document so that content is edge to edge-->
+  #  <xsl:template match="body/center/table/@width">
+  #       <xsl:attribute name="width">
+  #           <xsl:value-of select="'100%'"/>
+  #       </xsl:attribute>
+  #   </xsl:template>
+  #
+  $(".//table[1]") {
+    attribute("width", "100%")
+  } 
+    
 }
